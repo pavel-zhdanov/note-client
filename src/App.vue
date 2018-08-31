9ad07222c544bd853cec87d27ed77d672456883a
zhdanov <p.zhdanov1993@gmail.com>
2018-08-31 18:05:45 +0300

    Add toolbar and drower

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,65 @@
 <template>
   <v-app>
+
     <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
       app
-    >
+      temporary
+      v-model="drawer">
       <v-list>
         <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-        >
+          v-for="link of links"
+          :key="link.title"
+          :to="link.url">
           <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
+            <v-icon>{{link.icon}}</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+            <v-list-tile-title v-text="link.title"></v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar
-      app
-      :clipped-left="clipped"
-    >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
+
+    <v-toolbar app dark color="primary">
+      <v-toolbar-side-icon
+      @click="drawer=!drawer"
+      class="hidden-md-and-up"
+      ></v-toolbar-side-icon>
+      <v-toolbar-title>Note Client</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn
+          flat
+          v-for="link of links"
+          :key="link.title"
+          :to="link.url"
+        >
+          <v-icon left>{{link.icon}}</v-icon>
+          {{link.title}}</v-btn>
+      </v-toolbar-items>
     </v-toolbar>
+
     <v-content>
-      <router-view/>
+      <router-view></router-view>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
+
   </v-app>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire',
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
-    };
-  },
-  name: 'App',
-};
+  export default {
+    data() {
+      return {
+        links: [
+          { title: 'Login', icon: 'lock', url: '/login' },
+          { title: 'Registration', icon: 'face', url: '/registration' },
+          { title: 'Home', icon: 'home', url: '/' },
+          { title: 'Orders', icon: 'bookmark_border', url: '/orders' },
+          { title: 'New ad', icon: 'note_add', url: '/new' },
+          { title: 'My ads', icon: 'list', url: '/list' },
+        ],
+        drawer: false,
+      };
+    },
+  };
 </script>
